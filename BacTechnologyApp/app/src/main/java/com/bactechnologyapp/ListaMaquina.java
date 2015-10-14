e6b8da4df76e983d849a69272bf531a5040cc0b6
bakamedi <bakamedi12@gmail.com>
2015-10-14 12:04:23 -0500

    Filtro
    se añadio el filtro en el maquina y molde

--- BacTechnologyApp/app/src/main/java/com/bactechnologyapp/ListaMaquina.java
+++ BacTechnologyApp/app/src/main/java/com/bactechnologyapp/ListaMaquina.java
@@ -3,9 +3,12 @@ package com.bactechnologyapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -13,6 +16,7 @@ public class ListaMaquina extends AppCompatActivity {
 
     private String[] maquinas = {"Y-01/15 (HT-120)", "Y-02/15 (HT-120)", "Y-03 (NB-750) ", "Y-04 (750 B) NO EQUIPO HIDRAULICO ", "Y-05/15 (HT-200)","Y-06  (R-300)", "Y-07 (V-17)", "Y-10   (HT-200) ", "Y-11  (HT-160) ", "Y-12 (JM 368)","Y-13 (NB 190)","Y-14 (V-22)"};
     private ListView listaMaquina;
+    private EditText buscarMaquina;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +25,27 @@ public class ListaMaquina extends AppCompatActivity {
 
 
         listaMaquina = (ListView)findViewById(R.id.id_lista_maquina);
+        buscarMaquina = (EditText)findViewById(R.id.id_buscar_maquina);
 
-        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, maquinas);
+        final ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, maquinas);
         listaMaquina.setAdapter(adaptador);
+
+        buscarMaquina.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                adaptador.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         listaMaquina.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
