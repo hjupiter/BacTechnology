9a5abe4878b45c2e85ed007c106e5384283a20c2
bakamedi <bakamedi12@gmail.com>
2015-10-15 00:51:35 -0500

    Nueva carpeta

--- BacTechnologyApp/app/src/main/java/com/bactechnologyapp/MaquinaMolde.java
+++ BacTechnologyApp/app/src/main/java/com/bactechnologyapp/MaquinaMolde.java
@@ -1,15 +1,20 @@
 package com.bactechnologyapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MaquinaMolde extends AppCompatActivity {
+    private Intent ventanaCamara;
     private Button botonImagen;
-    private ImageView imagen;
+    private ImageView imagenCamara;
     private String idMolde;
     private String idMaquina;
     private Bitmap bmp;
@@ -21,9 +26,26 @@ public class MaquinaMolde extends AppCompatActivity {
 
         idMaquina = (String) getIntent().getExtras().getString("idMaquina");
         idMolde = (String) getIntent().getExtras().getString("idMolde");
+        imagenCamara = (ImageView)findViewById(R.id.id_imagen);
 
         TextView textId = (TextView) findViewById(R.id.textIdMaquinaMolde);
 
         textId.setText("Maquina : " + idMaquina + "\nMolde : " + idMolde);
     }
+
+    public void setBotonCamara(View v){
+        ventanaCamara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(ventanaCamara, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK){
+            Bundle extra = data.getExtras();
+            bmp = (Bitmap)extra.get("data");
+            imagenCamara.setImageBitmap(bmp);
+        }
+    }
+
 }
