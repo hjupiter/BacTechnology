afc2910cc01fbbda9f2f1797e0162ca70f0c7342
hjupiter <hector_franklin_17@yahoo.es>
2015-10-20 08:44:25 -0500

    Carga de moldes
    lista de moldes cargada en su correspondiente activity y carga de moldes
    para su uso

--- BacTechnologyApp/app/src/main/java/com/bactechnologyapp/ListaMaquina.java
+++ BacTechnologyApp/app/src/main/java/com/bactechnologyapp/ListaMaquina.java
@@ -1,6 +1,9 @@
 package com.bactechnologyapp;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -14,11 +17,21 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaMaquina extends AppCompatActivity {
 
-    private String[] maquinas = {"Y-01/15 (HT-120)", "Y-02/15 (HT-120)", "Y-03 (NB-750) ", "Y-04 (750 B) NO EQUIPO HIDRAULICO ", "Y-05/15 (HT-200)","Y-06  (R-300)", "Y-07 (V-17)", "Y-10   (HT-200) ", "Y-11  (HT-160) ", "Y-12 (JM 368)","Y-13 (NB 190)","Y-14 (V-22)"};
+    //private String[] maquinas = {"Y-01/15 (HT-120)", "Y-02/15 (HT-120)", "Y-03 (NB-750) ", "Y-04 (750 B) NO EQUIPO HIDRAULICO ", "Y-05/15 (HT-200)","Y-06  (R-300)", "Y-07 (V-17)", "Y-10   (HT-200) ", "Y-11  (HT-160) ", "Y-12 (JM 368)","Y-13 (NB 190)","Y-14 (V-22)"};
     private ListView listaMaquina;
+    private String[] maquinas;
     private EditText buscarMaquina;
+
+    private List moldes;
+    private ProgressDialog progressDialog;
+    private Context context;
+
+    private int pos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +40,9 @@ public class ListaMaquina extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        context = this;
+        maquinas = getIntent().getExtras().getStringArray("array");
 
         listaMaquina = (ListView)findViewById(R.id.id_lista_maquina);
         buscarMaquina = (EditText)findViewById(R.id.id_buscar_maquina);
@@ -56,14 +72,50 @@ public class ListaMaquina extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
                 // TODO Auto-generated method stub
-                String idMaquina = maquinas[position];
+                /*String idMaquina = maquinas[position];
                 Toast.makeText(getApplicationContext(), idMaquina, Toast.LENGTH_SHORT).show();
                 Intent newActivity = new Intent(ListaMaquina.this, ListaMolde.class);
                 newActivity.putExtra("idMaquina", idMaquina);
                 startActivity(newActivity);
+                */
+                pos = position;
+                new DownloadTask2().execute("");
+                progressDialog = ProgressDialog.show(context, "Por favor espere", "Autenticando");
             }
 
         });
 
+    }
+
+    private class DownloadTask2 extends AsyncTask<String,Void,Object> {
+
+        protected Integer doInBackground(String... args) {
+            cargaDatosWS ws = new cargaDatosWS();
+            moldes = new ArrayList();
+            moldes = ws.getMoldes();
+            return 1;
+        }
+
+        protected void onPostExecute(Object result) {
+            progressDialog.dismiss();
+
+            String idMaquina = maquinas[pos];
+            String[] arrayMoldes = convLisToArray(moldes);
+            Intent ventanaMoldes = new Intent(ListaMaquina.this, ListaMolde.class);
+            ventanaMoldes.putExtra("array", arrayMoldes);
+            ventanaMoldes.putExtra("idMaquina", idMaquina);
+            startActivity(ventanaMoldes);
+            super.onPostExecute(result);
+        }
+    }
+
+    private String[] convLisToArray(List list){
+        String [] array = null;
+        array =  new String[list.size()];
+        for(int i = 0;i<list.size();i++){
+            array[i] = list.get(i).toString();
+            System.out.println(array[i]);
+        }
+        return array;
     }
 }
