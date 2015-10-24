fe77c287555334e592fd36781025dc9a69f3e2fc
bakamedi <bakamedi12@gmail.com>
2015-10-24 14:16:30 -0500

    fix 2

--- GestionExcel/src/Base/DataBaseHandler.java
+++ GestionExcel/src/Base/DataBaseHandler.java
@@ -7,7 +7,6 @@ package Base;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Types;
