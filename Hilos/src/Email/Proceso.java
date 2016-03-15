b5962e412c512652b192ce7b7ef3159e35f6fe6d
bakamedi <bakamedi12@gmail.com>
2016-03-15 13:04:57 -0500

    Cambios en el Hilo para manejo de Correo
    implemente el token para que se generen mas de un archivo

--- Hilos/src/Email/Proceso.java
+++ Hilos/src/Email/Proceso.java
@@ -7,28 +7,17 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Types;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Properties;
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.activation.FileDataSource;
+import java.util.Random;
 import javax.imageio.ImageIO;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 import javax.swing.ImageIcon;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -46,33 +35,39 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  */
 public class Proceso extends Thread{
     private Email email;
+    private static Conexion conexion =  new Conexion();
+    private static Connection conn = conexion.Conexion();
     private int hora;
     private int min;
     private int sec;
+    private String dia,mes,annio,fecha;
+    private final Calendar c = Calendar.getInstance();
+    private String rutaArchivo = "C://data//";
+    private String nombreArchivo;
     
     public Proceso(String msg){
         super(msg);
     }
     
+    @Override
     public void run(){
         while(true){
             Date fecha = new Date();
             hora = fecha.getHours();
             min = fecha.getMinutes();
             sec = fecha.getSeconds();
-            //System.out.println(" hora "+hora+"-"+min+"-"+sec);
-            //obtenerDatosBase();
-            if(hora == 21 && min == 59 && sec == 0 ){
-                email = new Email("reportes.bactechnology.2016@gmail.com", "reportes.bactechnology.2016@gmail.com", "data.xlsx", "C://data//data.xlsx", "Reportes BacTechnology", "Por favor no responda a este mensaje");
+            if(hora == 12 && min == 22 && sec == 0 ){
                 obtenerDatosBase();
+                email = new Email("reportes.bactechnology.2016@gmail.com", "reportes.bactechnology.2016@gmail.com", getNombreArchivo(), getRutaArchivo(), "Reportes BacTechnology", "Por favor no responda a este mensaje");
                 email.sendMail();
             }
         }
     }
     
+    @SuppressWarnings("empty-statement")
     private void obtenerDatosBase(){
-        Conexion conexion =  new Conexion();
-        Connection conn = conexion.Conexion();
+        Random  rnd = new Random();
+        int token =  (int) rnd.nextInt(999999999);
         XSSFWorkbook wb = new XSSFWorkbook();
         XSSFSheet hoja = wb.createSheet();
         XSSFRow fila = hoja.createRow(0);
@@ -97,13 +92,10 @@ public class Proceso extends Thread{
         hoja.setColumnWidth(8, 256*15);
         hoja.setColumnWidth(9, 256*30);
         XSSFRow filas;
-        String dia,mes,annio,fecha;
-        Calendar c = Calendar.getInstance();
         dia = Integer.toString(c.get(Calendar.DATE));
         mes = Integer.toString(c.get(Calendar.MONTH)+1);
         annio = Integer.toString(c.get(Calendar.YEAR));
         fecha = ""+annio+"-"+mes+"-"+dia;
-        
         try{
             conn.setAutoCommit(false);
             CallableStatement todas_reportes =  conn.prepareCall("{ ? = CALL TODOS_REPORTES_FECHAS ( ? ) }");
@@ -166,30 +158,41 @@ public class Proceso extends Thread{
                         ImageIcon newicon = new ImageIcon(newimg);
                     }
                     else{
-                        String dato;
                         datos[i] = results.getObject(i+1);
-                        //System.out.println(" "+datos[i].toString());
                         filas.createCell(i).setCellValue(datos[i].toString());
-                    }   
-                    //datos[i] = results.getObject(i+1);
-                    //System.out.println(results.getObject(i+1));
+                    }
                 }
                 j++;
-                //System.out.println("----");
-                //System.out.println(datos[5]);
-                //System.out.println("----");
-                
             }
             todas_reportes.close();
             conn.close();
             try {
-                wb.write(new FileOutputStream(new File("C://data//data.xlsx")));
-                //Desktop.getDesktop().open(new File("C://data//data.xlsx"));
+                setRutaArchivo(rutaArchivo+fecha+"reportes"+token+".xlsx");
+                setNombreArchivo(fecha+"reportes"+token+".xlsx");
+                wb.write(new FileOutputStream(new File(getRutaArchivo())));
             } catch (Exception e) {
             }
-        }catch(Exception e){
+        }catch(SQLException | NumberFormatException | IOException e){
             System.out.println(e.getMessage());
         };
     }
+
+    private String getRutaArchivo() {
+        return rutaArchivo;
+    }
+
+    private void setRutaArchivo(String rutaArchivo) {
+        this.rutaArchivo = rutaArchivo;
+    }
+
+    private String getNombreArchivo() {
+        return nombreArchivo;
+    }
+
+    private void setNombreArchivo(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
+    
+    
     
 }
