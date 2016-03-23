e110365edc1ae06aabbb1c2e4a9bee367e0b42bf
bakamedi <bakamedi12@gmail.com>
2016-03-22 21:59:30 -0500

    Prueba 2

--- Web Service/BacTechnology/src/java/com/BacTechnology/service/BacTechnologyService.java
+++ Web Service/BacTechnology/src/java/com/BacTechnology/service/BacTechnologyService.java
@@ -5,27 +5,26 @@
  */
 package com.BacTechnology.service;
 
-import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
-import com.sun.xml.ws.org.objectweb.asm.Type;
+
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.Date;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.sql.Types;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import javax.imageio.ImageIO;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
-import org.jboss.weld.bean.builtin.CallableMethodHandler;
+import org.postgresql.util.Base64;
 
 /**
  *
@@ -36,6 +35,8 @@ public class BacTechnologyService {
 
     /**
      * This is a sample web service operation
+     * @param txt
+     * @return 
      */
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
@@ -46,23 +47,24 @@ public class BacTechnologyService {
 
     /**
      * Web service operation
+     * @return 
      */
     @WebMethod(operationName = "CargarMaquinarias")
     public List CargarMaquinarias() {
         //TODO write your implementation code here:
         Conexion conexion =  new Conexion();
         Connection conn = conexion.Conexion();
-        List lista = new ArrayList();
+        List<String> lista = new ArrayList<>();
         try{
             conn.setAutoCommit(false);
-            CallableStatement todas_maquinarias =  conn.prepareCall("{ ? = CALL TODAS_MAQUINARIAS ( ) }");
-            todas_maquinarias.registerOutParameter(1, Types.OTHER);
-            todas_maquinarias.execute();
-            ResultSet results = (ResultSet)todas_maquinarias.getObject(1);
-            while(results.next()){
-                lista.add(results.getString(2));
+            try (CallableStatement todas_maquinarias = conn.prepareCall("{ ? = CALL TODAS_MAQUINARIAS ( ) }")) {
+                todas_maquinarias.registerOutParameter(1, Types.OTHER);
+                todas_maquinarias.execute();
+                ResultSet results = (ResultSet)todas_maquinarias.getObject(1);
+                while(results.next()){
+                    lista.add(results.getString(2));
+                }
             }
-            todas_maquinarias.close();
             conn.close();
             return lista;
         }catch(Exception e){
@@ -73,28 +75,26 @@ public class BacTechnologyService {
 
     /**
      * Web service operation
+     * @param usuario
+     * @param contraseña
+     * @return 
      */
     @WebMethod(operationName = "Autenticacion")
-    public boolean Autenticacion(@WebParam(name = "usuario") String usuario, @WebParam(name = "contrasena") String contraseña) {
+    public Boolean Autenticacion(@WebParam(name = "usuario") String usuario, @WebParam(name = "contrasena") String contraseña) {
         //TODO write your implementation code here:
         Conexion conexion =  new Conexion();
         Connection conn = conexion.Conexion();
         try{
-            CallableStatement autenticacion =  conn.prepareCall("{ ? = call AUTENTICACION ( ? , ? ) }");
-            autenticacion.registerOutParameter(1, Types.INTEGER);
-            autenticacion.setString(2, usuario);
-            autenticacion.setString(3, contraseña);
-            autenticacion.execute();
-            int res = autenticacion.getInt(1);
-            System.out.println(res);
-            boolean rest;
-            autenticacion.close();
+            boolean res;
+            try (CallableStatement autenticacion = conn.prepareCall("{ ? = call AUTENTICACION_APP ( ? , ? ) }")) {
+                autenticacion.registerOutParameter(1, Types.BOOLEAN);
+                autenticacion.setString(2, usuario);
+                autenticacion.setString(3, contraseña);
+                autenticacion.execute();
+                res = autenticacion.getBoolean(1);
+            }
             conn.close();
-            if(res == 1 || res == 2)
-                rest = true;
-            else 
-                rest = false;
-            return rest;
+            return res;
         }catch(Exception e){
             return false;
         }
@@ -102,23 +102,24 @@ public class BacTechnologyService {
 
     /**
      * Web service operation
+     * @return 
      */
     @WebMethod(operationName = "CargarMoldes")
     public List CargarMoldes() {
         //TODO write your implementation code here:
         Conexion conexion =  new Conexion();
         Connection conn = conexion.Conexion();
-        List lista = new ArrayList();
+        List<String> lista = new ArrayList<>();
         try{
             conn.setAutoCommit(false);
-            CallableStatement todos_moldes =  conn.prepareCall("{ ? = CALL TODOS_MOLDES ( ) }");
-            todos_moldes.registerOutParameter(1, Types.OTHER);
-            todos_moldes.execute();
-            ResultSet results = (ResultSet)todos_moldes.getObject(1);
-            while(results.next()){
-                lista.add(results.getString(2));
+            try (CallableStatement todos_moldes = conn.prepareCall("{ ? = CALL TODOS_MOLDES ( ) }")) {
+                todos_moldes.registerOutParameter(1, Types.OTHER);
+                todos_moldes.execute();
+                ResultSet results = (ResultSet)todos_moldes.getObject(1);
+                while(results.next()){
+                    lista.add(results.getString(2));
+                }
             }
-            todos_moldes.close();
             conn.close();
             return lista;
         }catch(Exception e){
@@ -129,40 +130,56 @@ public class BacTechnologyService {
 
     /**
      * Web service operation
+     * @param foto
+     * @return 
      */
-    @WebMethod(operationName = "GuardaImagen")
-    public Boolean GuardaImagen(@WebParam(name = "foto") String foto) {
-        
-        byte[] imageInByte = Base64.decode(foto);
-        int longitudBytes = imageInByte.length;
-        
-        InputStream in = new ByteArrayInputStream(imageInByte);
-        
-        Conexion conexion =  new Conexion();
-        Connection conn = conexion.Conexion();
-        try{
-            CallableStatement imagen =  conn.prepareCall("{ ? = call imagen ( ? , ? ) }");
-            imagen.registerOutParameter(1, Types.BOOLEAN);
-            imagen.setBinaryStream(2,in,longitudBytes);
-            imagen.setString(3, "hola");
-            
-            imagen.execute();
-            boolean res = imagen.getBoolean(1);
-            imagen.close();
-            conn.close();
-            System.out.println(res);
-            return true;
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-            return false;
-        }
-    }
+//    @WebMethod(operationName = "GuardaImagen")
+//    public Boolean GuardaImagen(@WebParam(name = "foto") String foto) {
+//        byte[] imageInByte = Base64.decode(foto);
+//        int longitudBytes = imageInByte.length;
+//        InputStream in = new ByteArrayInputStream(imageInByte);
+//        Conexion conexion =  new Conexion();
+//        Connection conn = conexion.Conexion();
+//        try{
+//            CallableStatement imagen =  conn.prepareCall("{ ? = call imagen ( ? , ? ) }");
+//            imagen.registerOutParameter(1, Types.BOOLEAN);
+//            imagen.setBinaryStream(2,in,longitudBytes);
+//            imagen.setString(3, "hola");
+//            imagen.execute();
+//            boolean res = imagen.getBoolean(1);
+//            imagen.close();
+//            conn.close();
+//            System.out.println(res);
+//            return true;
+//        }catch(Exception e){
+//            System.out.println(e.getMessage());
+//            return false;
+//        }
+//    }
 
     /**
      * Web service operation
+     * @param maquina
+     * @param molde
+     * @param usuario
+     * @param novedad
+     * @param solucion
+     * @param tipoNovedad
+     * @param tipoSolucion
+     * @param foto
+     * @param novedadDetectada
+     * @return 
      */
     @WebMethod(operationName = "NuevoReporte")
-    public Boolean NuevoReporte(@WebParam(name = "maquina") String maquina, @WebParam(name = "molde") String molde, @WebParam(name = "usuario") String usuario, @WebParam(name = "novedad") String novedad, @WebParam(name = "solucion") String solucion, @WebParam(name = "tipoNovedad") String tipoNovedad, @WebParam(name = "tipoSolucion") String tipoSolucion, @WebParam(name = "foto") String foto, @WebParam(name = "novedadDetectada") String novedadDetectada) {
+    public Boolean NuevoReporte(@WebParam(name = "maquina") String maquina,
+                                @WebParam(name = "molde") String molde,
+                                @WebParam(name = "usuario") String usuario,
+                                @WebParam(name = "novedad") String novedad,
+                                @WebParam(name = "solucion") String solucion,
+                                @WebParam(name = "tipoNovedad") String tipoNovedad,
+                                @WebParam(name = "tipoSolucion") String tipoSolucion,
+                                @WebParam(name = "foto") String foto,
+                                @WebParam(name = "novedadDetectada") String novedadDetectada) {
         //TODO write your implementation code here:
         System.out.println(usuario);
         System.out.println(molde);
@@ -175,6 +192,7 @@ public class BacTechnologyService {
         System.out.println("------------------");
         Conexion conexion = new Conexion();
         Connection conn =  conexion.Conexion();
+        System.out.println("===================");
         try{
             CallableStatement funcion =  conn.prepareCall("{ ? = call buscar_usuario ( ? ) }");
             funcion.registerOutParameter(1, Types.INTEGER);
@@ -208,9 +226,12 @@ public class BacTechnologyService {
             funcion.close();
             
             System.out.println("----------");
-            
+            if(molde.equals("000-000")){
+                MoldeEcontrado = 1;
+            }
+            System.out.println(MoldeEcontrado);
             if(UsuarioEcontrado!=0 && MoldeEcontrado !=0 && MaquinariaEcontrado != 0){
-                System.out.println("asdasdasdsadsa");
+                System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
                 byte[] imageInByte = Base64.decode(foto);
                 int longitudBytes = imageInByte.length;
                 InputStream in = new ByteArrayInputStream(imageInByte);
@@ -228,18 +249,35 @@ public class BacTechnologyService {
                 funcion.setString(9, tipoSolucion);
                 funcion.setString(10, novedadDetectada);
                 funcion.setBinaryStream(11,in, longitudBytes);
-                funcion.setString(12, " ");
+                funcion.setString(12, "0");
                 funcion.execute();
                 System.out.println("asdasdasdafsadfsafasdfdsafasdfsadfasdfasd");
                 
-                /*
-                byte[] imageInByte1;
-                imageInByte1 = Base64.decode(foto);
-                InputStream inr = new ByteArrayInputStream(imageInByte1);
-                BufferedImage bImageFromConvert1 = ImageIO.read(inr);
-                ImageIO.write(bImageFromConvert1, "jpg", new File(
-                            "c:/Users/Angel/Desktop/new-darksouls.jpg"));
-                */
+                
+                Random  rnd = new Random();
+                int token =  (int) rnd.nextInt(999999999);
+
+
+                String directorio = "C:/Bac-Technology System Image/"+usuario+"/"+fecha+"/";
+                File crear_carpeta = new File(directorio);
+//                if(crear_carpeta.exists()){
+//                    //JOptionPane.showMessageDialog(null, "El archivo ya existe");
+//                    byte[] imageInByte1;
+//                    imageInByte1 = Base64.decode(foto);
+//                    InputStream inr = new ByteArrayInputStream(imageInByte1);
+//                    BufferedImage bImageFromConvert1 = ImageIO.read(inr);
+//                    ImageIO.write(bImageFromConvert1, "jpg", new File("C:/Bac-Technology System Image/"+usuario+"/"+fecha+"/"+fecha+"-"+token+"foto.jpg"));
+//                }else{
+//                    //JOptionPane.showMessageDialog(null, "no esxite se va a crear...");
+//                    crear_carpeta.mkdirs();
+//                    byte[] imageInByte1;
+//                    imageInByte1 = Base64.decode(foto);
+//                    InputStream inr = new ByteArrayInputStream(imageInByte1);
+//                    BufferedImage bImageFromConvert1 = ImageIO.read(inr);
+//                    ImageIO.write(bImageFromConvert1, "jpg", new File("C:/Bac-Technology System Image/"+usuario+"/"+fecha+"/"+fecha+"-"+token+"foto.jpg"));
+//                }
+                
+                
                 boolean a = funcion.getBoolean(1);
                 System.out.println(a);
                 funcion.close();
@@ -250,6 +288,7 @@ public class BacTechnologyService {
             
             
         }catch(Exception e){
+            System.out.println("NO SE INGRESO LOS DATOS");
             return false;
         }
         return null;
@@ -258,9 +297,29 @@ public class BacTechnologyService {
     
     /**
      * Web service operation
+     * @param maquina
+     * @param molde
+     * @param usuario
+     * @param novedad
+     * @param solucion
+     * @param tipoNovedad
+     * @param tipoSolucion
+     * @param foto
+     * @param novedadDetectada
+     * @param articulo
+     * @return 
      */
     @WebMethod(operationName = "NuevoReporteMolde")
-    public Boolean NuevoReporteMolde(@WebParam(name = "maquina") String maquina, @WebParam(name = "molde") String molde, @WebParam(name = "usuario") String usuario, @WebParam(name = "novedad") String novedad, @WebParam(name = "solucion") String solucion, @WebParam(name = "tipoNovedad") String tipoNovedad, @WebParam(name = "tipoSolucion") String tipoSolucion, @WebParam(name = "foto") String foto, @WebParam(name = "novedadDetectada") String novedadDetectada, @WebParam(name = "articulo") String articulo) {
+    public Boolean NuevoReporteMolde(@WebParam(name = "maquina") String maquina, 
+                                     @WebParam(name = "molde") String molde,
+                                     @WebParam(name = "usuario") String usuario,
+                                     @WebParam(name = "novedad") String novedad,
+                                     @WebParam(name = "solucion") String solucion,
+                                     @WebParam(name = "tipoNovedad") String tipoNovedad,
+                                     @WebParam(name = "tipoSolucion") String tipoSolucion,
+                                     @WebParam(name = "foto") String foto,
+                                     @WebParam(name = "novedadDetectada") String novedadDetectada,
+                                     @WebParam(name = "articulo") String articulo) {
         //TODO write your implementation code here:
         System.out.println("Usuario: "+usuario);
         System.out.println("Molde: "+molde);
@@ -274,6 +333,7 @@ public class BacTechnologyService {
         System.out.println("------------------");
         Conexion conexion = new Conexion();
         Connection conn =  conexion.Conexion();
+        System.out.println("===================");
         try{
             CallableStatement funcion =  conn.prepareCall("{ ? = call buscar_usuario ( ? ) }");
             funcion.registerOutParameter(1, Types.INTEGER);
@@ -327,19 +387,37 @@ public class BacTechnologyService {
                 funcion.setString(10, novedadDetectada);
                 funcion.setBinaryStream(11,in, longitudBytes);
                 funcion.setString(12,articulo);
-                funcion.setString(13," ");
+                funcion.setString(13,"0");
                 System.out.println("----------------->>>>>>");
                 funcion.execute();
                 System.out.println("asdasdasdafsadfsafasdfdsafasdfsadfasdfasd");
                 
-                /*
-                byte[] imageInByte1;
-                imageInByte1 = Base64.decode(foto);
-                InputStream inr = new ByteArrayInputStream(imageInByte1);
-                BufferedImage bImageFromConvert1 = ImageIO.read(inr);
-                ImageIO.write(bImageFromConvert1, "jpg", new File(
-                            "c:/Users/Angel/Desktop/new-darksouls.jpg"));
-                */
+                
+                //C:\Users\bakamedi\Desktop\Test
+//                String directorio = "C:/Bac-Technology System Image/nueva/";
+//                String archivo = "foto.jpg";
+//                File crear_carpeta = new File(directorio);
+//                File crea_archivo = new File(directorio+archivo);
+//                if(crea_archivo.exists()){
+//                    //JOptionPane.showMessageDialog(null, "El archivo ya existe");
+//                }else{
+//                    //JOptionPane.showMessageDialog(null, "no esxite se va a crear...");
+//                    crear_carpeta.mkdirs();
+//                    try {
+//                        if(crea_archivo.createNewFile()){
+//                            byte[] imageInByte1;
+//                            imageInByte1 = Base64.decode(foto);
+//                            InputStream inr = new ByteArrayInputStream(imageInByte1);
+//                            BufferedImage bImageFromConvert1 = ImageIO.read(inr);
+//                            ImageIO.write(bImageFromConvert1, "jpg", crea_archivo);
+//                            System.out.println("Arcchivo creado..");
+//                            //JOptionPane.showMessageDialog(null, "Archivo creado");
+//                        }
+//                    } catch (IOException ex) {
+//                        //JOptionPane.showMessageDialog(null, "Archivo no creado");
+//                    }
+//                }
+                
                 boolean a = funcion.getBoolean(1);
                 System.out.println(a);
                 funcion.close();
@@ -355,5 +433,3 @@ public class BacTechnologyService {
         return null;
     }
 }
-
-
