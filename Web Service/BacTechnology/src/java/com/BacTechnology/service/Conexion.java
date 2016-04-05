32a3a0304b398b1474a716c8be46a292b1abc323
bakamedi <bakamedi12@gmail.com>
2016-04-05 14:28:44 -0500

    Proteccion de la IP
    puse como static y final para que el programa no modifique las ip

--- Web Service/BacTechnology/src/java/com/BacTechnology/service/Conexion.java
+++ Web Service/BacTechnology/src/java/com/BacTechnology/service/Conexion.java
@@ -5,34 +5,28 @@
  */
 package com.BacTechnology.service;
 
-import java.sql.CallableStatement;
+package com.BacTechnology.service;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.Types;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.SQLException;
+
 
 /**
  *
  * @author Angel
  */
 public class Conexion {
-    
-    
+    private Connection conexion = null;
+    private static final String cadena = "jdbc:postgresql://192.168.1.104:5432/BACTECHNOLOGY";
+    private static final String user = "postgres";
+    private static final String pass = "root";
     public Connection Conexion(){
-        List resultData =  new ArrayList();
-        Connection conexion = null;
         try{
-            String cadena = "jdbc:postgresql://192.168.1.6:5432/BACTECHNOLOGY";
-            String user = "postgres";
-            String pass = "root";
             Class.forName("org.postgresql.Driver");
             conexion = DriverManager.getConnection(cadena,user,pass);
             return conexion;
-        }catch(Exception e){
+        }catch(ClassNotFoundException | SQLException e){
             return conexion;
         }
-    }
+    }  
 }
